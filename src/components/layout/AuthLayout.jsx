d46c446c2cdfbf59340a519340e2ad742a66ba1c
Rohit Kumar Solanki <128624164+rohitsolanki401@users.noreply.github.com>
2025-05-23 15:50:50 +0530

    Delete src directory

--- src/components/layout/AuthLayout.jsx
+++
@@ -1,7 +0,0 @@
-// src/layouts/AuthLayout.jsx
-import React from "react";
-import "./AuthLayout.css";
-
-export default function AuthLayout({ children }) {
-  return <div className="auth-wrapper">{children}</div>;
-}
